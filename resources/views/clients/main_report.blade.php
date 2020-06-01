936699ade324f254b16e206ae47c6b38ac72b482
Mina Nabil <mina9492@hotmail.com>
2020-06-01 18:25:55 +0200

    Client etshal tany

--- resources/views/clients/main_report.blade.php
+++ resources/views/clients/main_report.blade.php
@@ -8,7 +8,7 @@
                 <h4 class="card-title">Main Clients Report</h4>
                 <h6 class="card-subtitle">Show Clients totals during the selected period without online clients</h6>
                 <div class="table-responsive m-t-40">
-                    <table id="myTable" class="table color-bordered-table table-striped full-color-table full-info-table hover-table" data-display-length='25' data-order="[]" >
+                    <table id="myTable" class="table color-bordered-table table-striped full-color-table full-info-table hover-table" data-display-length='-1' data-order="[]">
                         <thead>
                             <tr>
                                 <th>اسم</th>
@@ -36,7 +36,7 @@
                                 <td>{{number_format($op->totalDisc, 2)}}</td>
                                 <td>{{number_format($op->totalReturn, 2)}}</td>
                                 <td>{{number_format($ops['balances'][$op->id], 2)}}</td>
-                            </tr> 
+                            </tr>
                             @endforeach
                             @foreach($ops['others'] as $op)
                             <tr>
@@ -52,14 +52,15 @@
                                 <td>0</td>
                                 <td>0</td>
                                 <td>{{number_format($op->CLTR_BLNC, 2)}}</td>
-                            </tr> 
+                            </tr>
                             @endforeach
-                        </tbody>
-                        @if(isset($ops['totals']))
-                        <tfoot>
-                            <tr>
+
+                            @if(isset($ops['totals']))
+
+                            <tr class="table-info">
                                 <td><strong>Totals</strong></td>
-                                <td><strong>{{number_format($ops['totals']->totalBalance - $ops['totals']->totalPurch + $ops['totals']->totalCash + $ops['totals']->totalDisc + $ops['totals']->totalNotes + $ops['totals']->totalReturn, 2)}}</strong></td>
+                                <td><strong>{{number_format($ops['totals']->totalBalance - $ops['totals']->totalPurch + $ops['totals']->totalCash + $ops['totals']->totalDisc + $ops['totals']->totalNotes + $ops['totals']->totalReturn, 2)}}</strong>
+                                </td>
                                 <td><strong>{{number_format($ops['totals']->totalPurch, 2)}}</strong></td>
                                 <td><strong>{{number_format($ops['totals']->totalCash, 2)}}</strong></td>
                                 <td><strong>{{number_format($ops['totals']->totalNotes, 2)}}</strong></td>
@@ -67,32 +68,10 @@
                                 <td><strong>{{number_format($ops['totals']->totalReturn, 2)}}</strong></td>
                                 <td><strong>{{number_format($ops['totals']->totalBalance, 2)}}</strong></td>
                             </tr>
-                        </tfoot>
-                        @endif
-                    </table>
-                </div>
-            </div>
-        </div>
-    
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Online Clients Report</h4>
-                <h6 class="card-subtitle">Show Clients totals during the selected period for online clients only</h6>
-                <div class="table-responsive m-t-40">
-                    <table id="myTable2" class="table color-bordered-table table-striped full-color-table full-info-table hover-table" data-display-length='25' data-order="[]" >
-                        <thead>
-                            <tr>
-                                <th>اسم</th>
-                                <th>رصيد مبدئي</th>
-                                <th>مبيعات</th>
-                                <th>نقديه</th>
-                                <th>اوراق دفع</th>
-                                <th>خصم</th>
-                                <th>مرتجع</th>
-                                <th>رصيد نهائي</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+
+                            @endif
+
+
                             @foreach($onlineOps['data'] as $op)
                             <tr>
                                 <td>
@@ -107,7 +86,7 @@
                                 <td>{{number_format($op->totalDisc, 2)}}</td>
                                 <td>{{number_format($op->totalReturn, 2)}}</td>
                                 <td>{{number_format($onlineOps['balances'][$op->id], 2)}}</td>
-                            </tr> 
+                            </tr>
                             @endforeach
                             @foreach($onlineOps['onlineOthers'] as $op)
                             <tr>
@@ -123,14 +102,15 @@
                                 <td>0</td>
                                 <td>0</td>
                                 <td>{{number_format($op->CLTR_BLNC, 2)}}</td>
-                            </tr> 
+                            </tr>
                             @endforeach
-                        </tbody>
-                        @if(isset($onlineOps['onlineTotals']))
-                        <tfoot>
-                            <tr>
+
+                            @if(isset($onlineOps['onlineTotals']))
+
+                            <tr class="table-info">
                                 <td><strong>Totals</strong></td>
-                                <td><strong>{{number_format($onlineOps['onlineTotals']->totalBalance - $onlineOps['onlineTotals']->totalPurch + $onlineOps['onlineTotals']->totalCash + $onlineOps['onlineTotals']->totalDisc + $onlineOps['onlineTotals']->totalNotes + $onlineOps['onlineTotals']->totalReturn, 2)}}</strong></td>
+                                <td><strong>{{number_format($onlineOps['onlineTotals']->totalBalance - $onlineOps['onlineTotals']->totalPurch + $onlineOps['onlineTotals']->totalCash + $onlineOps['onlineTotals']->totalDisc + $onlineOps['onlineTotals']->totalNotes + $onlineOps['onlineTotals']->totalReturn, 2)}}</strong>
+                                </td>
                                 <td><strong>{{number_format($onlineOps['onlineTotals']->totalPurch, 2)}}</strong></td>
                                 <td><strong>{{number_format($onlineOps['onlineTotals']->totalCash, 2)}}</strong></td>
                                 <td><strong>{{number_format($onlineOps['onlineTotals']->totalNotes, 2)}}</strong></td>
@@ -138,13 +118,14 @@
                                 <td><strong>{{number_format($onlineOps['onlineTotals']->totalReturn, 2)}}</strong></td>
                                 <td><strong>{{number_format($onlineOps['onlineTotals']->totalBalance, 2)}}</strong></td>
                             </tr>
-                        </tfoot>
-                        @endif
+
+                            @endif
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
-    
+
     </div>
 </div>
 @endsection
