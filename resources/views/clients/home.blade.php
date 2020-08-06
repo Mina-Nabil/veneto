c76ce728b893a3c773393da92924fa34349495e1
Mina Nabil <mina9492@hotmail.com>
2020-08-06 13:16:34 +0200

    adding client types

--- resources/views/clients/home.blade.php
+++ resources/views/clients/home.blade.php
@@ -8,7 +8,7 @@
                 <h4 class="card-title">Clients</h4>
                 <h6 class="card-subtitle">Show All Clients data</h6>
                 <div class="table-responsive m-t-40">
-                    <table id="myTable" class="table color-bordered-table table-striped full-color-table full-info-table hover-table" data-display-length='-1' data-order="[]" >
+                    <table id="myTable" class="table color-bordered-table table-striped full-color-table full-info-table hover-table" data-display-length='-1' data-order="[]">
                         <thead>
                             <tr>
                                 <th>#</th>
@@ -29,16 +29,18 @@
                                 </td>
                                 <td>{{$client->CLNT_ARBC_NAME}}</td>
                                 <td>{{number_format($client->CLNT_BLNC, 2)}}</td>
-                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>                               
-                            </tr> 
+                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>
+                            </tr>
                             @endforeach
-                            <tfoot>
-                                <tr>
-                                <td colspan=2><strong>Total Balance: </strong></td>
+
+                            <tr>
+                                <td></td>
+                                <td><strong>Veneto Balance: </strong></td>
+                                <td></td>
                                 <td><strong>{{number_format($totalVeneto, 2)}}</strong></td>
                                 <td></td>
-                                </tr>
-                            </tfoot>
+
+                            </tr>
                             @foreach($online as $client)
                             <tr>
                                 <td>{{$client->CLNT_SRNO}}</td>
@@ -49,16 +51,18 @@
                                 </td>
                                 <td>{{$client->CLNT_ARBC_NAME}}</td>
                                 <td>{{number_format($client->CLNT_BLNC, 2)}}</td>
-                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>                               
-                            </tr> 
+                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>
+                            </tr>
                             @endforeach
-                            <tfoot>
-                                <tr>
-                                <td colspan=2><strong>Total Balance: </strong></td>
+
+                            <tr>
+                                <td></td>
+                                <td><strong>Online Balance: </strong></td>
+                                <td></td>
                                 <td><strong>{{number_format($totalOnline, 2)}}</strong></td>
                                 <td></td>
-                                </tr>
-                            </tfoot>
+
+                            </tr>
                             @foreach($via as $client)
                             <tr>
                                 <td>{{$client->CLNT_SRNO}}</td>
@@ -69,16 +73,18 @@
                                 </td>
                                 <td>{{$client->CLNT_ARBC_NAME}}</td>
                                 <td>{{number_format($client->CLNT_BLNC, 2)}}</td>
-                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>                               
-                            </tr> 
+                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>
+                            </tr>
                             @endforeach
-                            <tfoot>
-                                <tr>
-                                <td colspan=2><strong>Total Balance: </strong></td>
+
+                            <tr>
+                                <td></td>
+                                <td><strong>Via Veneto Balance: </strong></td>
+                                <td></td>
                                 <td><strong>{{number_format($totalVia, 2)}}</strong></td>
                                 <td></td>
-                                </tr>
-                            </tfoot>
+
+                            </tr>
                             @foreach($prod as $client)
                             <tr>
                                 <td>{{$client->CLNT_SRNO}}</td>
@@ -89,16 +95,18 @@
                                 </td>
                                 <td>{{$client->CLNT_ARBC_NAME}}</td>
                                 <td>{{number_format($client->CLNT_BLNC, 2)}}</td>
-                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>                               
-                            </tr> 
+                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>
+                            </tr>
                             @endforeach
-                            <tfoot>
-                                <tr>
-                                <td colspan=2><strong>Total Balance: </strong></td>
+
+                            <tr>
+                                <td></td>
+                                <td><strong>Production Balance: </strong></td>
+                                <td></td>
                                 <td><strong>{{number_format($totalProd, 2)}}</strong></td>
                                 <td></td>
-                                </tr>
-                            </tfoot>
+
+                            </tr>
                             @foreach($proc as $client)
                             <tr>
                                 <td>{{$client->CLNT_SRNO}}</td>
@@ -109,22 +117,24 @@
                                 </td>
                                 <td>{{$client->CLNT_ARBC_NAME}}</td>
                                 <td>{{number_format($client->CLNT_BLNC, 2)}}</td>
-                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>                               
-                            </tr> 
+                                <td><a href="{{ url('clients/edit/' . $client->id) }}"><img src="{{ asset('images/edit.png') }}" width=25 height=25></a></td>
+                            </tr>
                             @endforeach
-                            <tfoot>
-                                <tr>
-                                <td colspan=2><strong>Total Balance: </strong></td>
+                            <tr>
+                                <td></td>
+                                <td><strong>Procurement Balance: </strong></td>
+                                <td></td>
                                 <td><strong>{{number_format($totalProc, 2)}}</strong></td>
                                 <td></td>
-                                </tr>
-                            </tfoot>
+                            </tr>
                         </tbody>
                         <tfoot>
                             <tr>
-                            <td colspan=2><strong>Total Balance: </strong></td>
-                            <td><strong>{{number_format($total, 2)}}</strong></td>
-                            <td></td>
+                                <td></td>
+                                <td><strong>Total Balance: </strong></td>
+                                <td></td>
+                                <td><strong>{{number_format($total, 2)}}</strong></td>
+                                <td></td>
                             </tr>
                         </tfoot>
                     </table>
