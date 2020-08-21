739357ed996a08b2c63b8ec6074c540242125f4e
Mina Nabil <mina9492@hotmail.com>
2020-08-21 22:02:57 +0200

    Fixing comment button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -365,6 +365,7 @@
     <!-- ============================================================== -->
     <script src="{{ asset('assets/node_modules/jquery/jquery-3.2.1.min.js') }}"></script>
     <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset('assets/node_modules/popper/popper.min.js') }}"></script>
     <script src="{{ asset('assets/node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <!-- slimscrollbar scrollbar JavaScript -->
     <script src="{{ asset('dist/js/perfect-scrollbar.jquery.min.js') }}"></script>
