cad206a9f2ba3b1a0aee261a0e1d778ba0107c93
Mina Nabil <mina9492@hotmail.com>
2020-09-29 23:22:03 +0200

    Suppliers types totals

--- app/Suppliers.php
+++ app/Suppliers.php
@@ -65,8 +65,10 @@ class Suppliers extends Model
         }
 
         $balances = DB::table("supplier_trans as t1")->selectRaw("t1.id, SPTR_SUPP_ID , SPTR_BLNC , SPTR_DATE")
-            ->havingRaw("id = (SELECT max(t2.id) from supplier_trans as t2 JOIN suppliers ON t2.SPTR_SUPP_ID = suppliers.id WHERE  {$balancesWhereString} ) ")
-            ->get();
+            ->havingRaw("id = (SELECT max(t2.id) from supplier_trans as t2 JOIN suppliers ON t2.SPTR_SUPP_ID = suppliers.id WHERE  {$balancesWhereString} ) ");
+            dd($balances->toSql());
+
+            $balances->get();
       
         $ret['balances'] = [];
         foreach ($balances as $balance) {
@@ -75,7 +77,7 @@ class Suppliers extends Model
 
         $ret['others'] = DB::table("suppliers as t1")->join('supplier_trans', "SPTR_SUPP_ID", "=", "t1.id")
             ->select(['t1.id', 'SPTR_BLNC', 'SUPP_NAME'])->whereNotIn('t1.id', $balances->pluck('SPTR_SUPP_ID'))
-            ->whereRaw(" supplier_trans.id = (SELECT MAX(id) FROM supplier_trans HAVING SPTR_SUPP_ID = t1.id AND SPTR_DATE <= '{$to}' ) ");
+            ->whereRaw(" supplier_trans.id = (SELECT MAX(id) FROM supplier_trans WHERE SPTR_SUPP_ID = t1.id AND SPTR_DATE <= '{$to}' ) ");
 
         if ($type == -1) {
             $ret['others'] = $ret['others']->get();
