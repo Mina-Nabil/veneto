9eb8a58d192ab4d615c5f3709e55b10768f62487
Mina Nabil <mina9492@hotmail.com>
2020-11-08 10:43:40 +0200

    Client Sort fix

--- app/Clients.php
+++ app/Clients.php
@@ -121,7 +121,7 @@ class Clients extends Model
 
         $ret['data']->merge($ret['others']);
         $ret['data']->sortBy(function ($a, $b) {
-            dd($a);
+            dd($b);
             if ($a->CLNT_SRNO == $b->CLNT_SRNO) {
                 return 0;
             }
